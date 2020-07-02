237c4f965edf0f11cf9a550a7efc1d215c46506b
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2020-07-02 15:54:01 +0100

    fix event log format

--- src/incident_log.ts
+++ src/incident_log.ts
@@ -3,7 +3,7 @@ import { IncidentEvent } from "./types/incident_event";
 export function generateIncidentLog(events: IncidentEvent[]): string {
   return events
     .map(x => {
-      return `${x.time} - ${x.user} - ${x.log}`;
+      return `${x.time.format("YYYY-MM-DD HH:MM")} - ${x.user} - ${x.log}`;
     })
     .reduce((x, y) => {
       return x + "\n" + y;
